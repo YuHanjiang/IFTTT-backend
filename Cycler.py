174bcfb8ba564663ef21fff98eba1c5f9f745309
Amby <nagubuzo@gmail.com>
2020-10-08 19:34:06 -0400

    updates

--- Cycler.py
+++ Cycler.py
@@ -1,18 +1,26 @@
 class Cycler: 
     def __init__(self):
         self.checkers = [] #collection of threads that will run checker functions
-        self.relations = []
+        self.relations = [] # relations  
+
+         
+
     #creates the relation objects based off of the database form sever-admin side 
     def load_from_dataBase(self):
         return None 
 
-    def start_checkers(self): 
+    def start_checkers(self):  
+        for relation in relations: 
+            #make new thread with checker fucntion and pass relation in  
+            #add thread to checker list 
+            
         for checker in self.checkers: 
-            checker.start(); 
+            checker.start();  
+
 
     def monitor(self): 
         for relation in relations: 
-            if isPulled = True: 
+            if isPulled == True: 
                 self.sendOutputSource(relation) 
          
     def sendOutputSource(self, relation): 

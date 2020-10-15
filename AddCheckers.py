c0ec7a8ef0e448cfda7952294741bbf586788481
Hanjiang Yu <yuhanjiang0417@gmail.com>
2020-10-15 18:01:54 -0400

    Create AddCheckers.py

---
+++ AddCheckers.py
@@ -0,0 +1,11 @@
+# Some Checker function that returns a boolean value to check if a condition is met
+def check_abc(a, b):
+    return True
+
+
+def check_xyz(x, y):
+    return False
+
+
+# A list of functions in the AddChecker
+list_functions = [check_abc(), check_xyz()]

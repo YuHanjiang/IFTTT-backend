4f2667dd8641d988e5f691c4fc61630eb39f2186
Hanjiang Yu <yuhanjiang0417@gmail.com>
2020-11-12 15:36:50 -0500

    Revert "Update WeatherMonitor.py"
    This reverts commit 2c2c91a1ea7be752e21c71c17fdd6e464409e76f.

--- Monitors/WeatherMonitor.py
+++ Monitors/WeatherMonitor.py
@@ -1,31 +1,27 @@
 from Monitor import Monitor
-import json
+import json 
 import requests
+monitor_var = ["Temp"] 
 
-monitor_var = ["Temp"]
-
-
-class WeatherMonitor(Monitor):
+class WeatherMonitor(Monitor): 
 
     def _mapper(self):
         varToFuncMapping = {'Temp': self._temp_check}
 
         return varToFuncMapping
 
-    def _temp_check(self, func, val):
-        try:
-            r = requests.get("http://" + self.src)
-            if r.status_code == 200:
-                temp = float(json.loads(r.json())["main"]["temp"])
-                return func(temp, val)
-            else:
-                return True
+    def _temp_check(self, func, val): 
+        try: 
+            r = requests.get("http://" + self.src)  
+            temp = float(json.loads(r.json)["main"]["temp"]) 
+            return func(temp,val) 
+             
 
         except requests.exceptions.RequestException as err:
-            print('Access Denied')
-            return True
-
+                print('Access Denied')
+                return True 
 
 def start(trigger):
     monitor = WeatherMonitor(trigger)
-    monitor.run()
+    monitor.run()    
+

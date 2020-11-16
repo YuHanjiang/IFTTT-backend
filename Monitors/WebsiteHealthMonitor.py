def932288c9583930a777c5e5c5f8d487fb25fbc
Hanjiang Yu <yuhanjiang0417@gmail.com>
2020-11-16 16:35:34 -0500

    Update website health monitor

--- Monitors/WebsiteHealthMonitor.py
+++ Monitors/WebsiteHealthMonitor.py
@@ -24,6 +24,8 @@ class WebsiteHealthChecker(Monitor):
         return func(int(r.status_code), val)
 
     def _ping_check(self, func, val):
+        print(self.src)
+        print(val)
         r = pythonping.ping(self.src, size=50)
         a = r.rtt_avg_ms
         return func(float(a), val)

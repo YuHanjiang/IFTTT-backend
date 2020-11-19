80e39d121edae31aef2b525dcea69ba2caaa1b5b
Hanjiang Yu <yuhanjiang0417@gmail.com>
2020-11-19 03:37:09 -0500

    Update ServerIO.py

--- ServerIO.py
+++ ServerIO.py
@@ -113,8 +113,9 @@ class ServerIO:
 
         # Send HTTP request to the api to notify trigger addition
         try:
-            # send_json = json.dumps({'trigger_id': triggerId})
-            r = requests.post('http://vocation.cs.umd.edu/flask/api/trigger_added', data=[str(triggerId)])
+            send_json = json.dumps({"trigger_id": triggerId})
+            r = requests.post('http://vocation.cs.umd.edu/flask/api/trigger_added', data=[str(triggerId)],
+                              json=send_json)
             print(r.status_code)
         except requests.exceptions.RequestException as e:
             print('Contact the API.')

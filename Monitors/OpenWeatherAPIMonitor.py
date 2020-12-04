962a2cbc7a455019a7087caa004f2e6131599d81
Hanjiang Yu <yuhanjiang0417@gmail.com>
2020-12-04 00:02:07 -0500

    Modified url handling

--- Monitors/OpenWeatherAPIMonitor.py
+++ Monitors/OpenWeatherAPIMonitor.py
@@ -16,39 +16,29 @@ class WeatherMonitor(Monitor):
         try:
             r = requests.get("http://" + self.src)
             if r.status_code == 200:
-                try:
-                    weather_json = r.json()
-                    if 'main' in weather_json and 'temp' in weather_json['main']:
-                        temp = float(weather_json['main']['temp'])
-                        return func(temp, val)
-                    else:
-                        return True
-                except Exception as ke:
-                    return True
-            else:
-                return True
-
+                weather_json = r.json()
+                if 'main' in weather_json and 'temp' in weather_json['main']:
+                    temp = float(weather_json['main']['temp'])
+                    return func(temp, val)
         except requests.exceptions.RequestException as err:
+            self.trigger.terminated = True
             return True
 
     def _humidity_check(self, func, val):
         try:
             r = requests.get("http://" + self.src)
             if r.status_code == 200:
-                try:
-                    weather_json = r.json()
-                    if 'main' in weather_json and 'humidity' in weather_json['main']:
-                        hum = weather_json['main']['humidity']
-                        return func(hum, val)
-                    else:
-                        return True
-
-                except Exception as ke:
-                    return True
-            else:
-                return True
+                weather_json = r.json()
+                if 'main' in weather_json and 'humidity' in weather_json['main']:
+                    hum = weather_json['main']['humidity']
+                    return func(hum, val)
 
         except requests.exceptions.RequestException as err:
+            self.trigger.terminated = True
+            return True
+
+        except KeyError as key_err:
+            print('Json Key Error: ', self.triggerId)
             return True
 
 
---
+++ Monitors/PolygonStockAPIMonitor.py
@@ -0,0 +1,30 @@
+from Monitor import Monitor
+import MonitorVarTypes
+import requests
+
+monitor_var = {'Previous Closing Price': MonitorVarTypes.FLOAT}
+
+
+class PolygonStockAPIMonitor(Monitor):
+
+    def _mapper(self):
+        return {'Previous Closing Price': self._price_check}
+
+    def _price_check(self, func, val):
+        try:
+            r = requests.get(self.src)
+            if r.status_code == 200:
+                stock_json = r.json()
+                closing_price = stock_json['results'][0]['c']
+                return func(closing_price, val)
+
+        except Exception as err:
+            self.trigger.terminated = True
+            print('URL Error: ', self.triggerId)
+            return True
+
+
+def start(trigger):
+    monitor = PolygonStockAPIMonitor(trigger)
+    monitor.run()
+

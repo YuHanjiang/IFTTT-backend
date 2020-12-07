c86312455a0458cec3fbdf26e7f3cc7b462f3141
Hanjiang Yu <yuhanjiang0417@gmail.com>
2020-12-06 20:47:00 -0500

    Revert "Added instance variable alarm_sounded to track trigger status"
    This reverts commit 82c55cf9d5e810a63d5538e9c1cc9c63e2ee2744.

--- Monitor.py
+++ Monitor.py
@@ -37,23 +37,23 @@ class Monitor:
 
             if result:
                 # if it is active sound alarm
-                if active == 1 and self.alarm_sounded is False:
+                if active == 1:
                     print(self.triggerId, 'Alert', sep=' ')
                     self.serverIO.pushNotification(self.triggerId, self.trigger_owner, self.trigger, clause_met)
-                    self.alarm_sounded = True
+                    self.need_to_change_status = True
                     # active = 0
             else:
                 # if not active switch back to active
-                if active == 0 and self.alarm_sounded is True:
+                if active == 0 and self.need_to_change_status is True:
                     self.serverIO.setBackToActive(self.triggerId)
-                    self.alarm_sounded = False
+                    self.need_to_change_status = False
                 print(self.triggerId, 'Passed', sep=' ')
             time.sleep(self.interval)
 
     def __init__(self, trigger):
         self.conditionMet = False
         self.serverIO = ServerIO()
-        self.alarm_sounded = False
+        self.need_to_change_status = False
 
         if trigger is not None:
             self.trigger = trigger
